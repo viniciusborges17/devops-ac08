84e1c5b04511aca9cef1482921e5791b7e100c80
VitorLorente <30847617+VitorLorente@users.noreply.github.com>
2017-11-07 20:47:45 -0200

    Update models.py

--- app/models.py
+++ app/models.py
@@ -13,3 +13,5 @@ class Curso(models.Model):
 class Vestibular(models.Model):
     nome = models.CharField(max_length=200)
 
+class Candidato(models.Model):
+    nome = models.Charfield(max_length=280)
