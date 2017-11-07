e2f3e15534decb95cfaae076806aebdddc8573b5
VitorLorente <30847617+VitorLorente@users.noreply.github.com>
2017-11-07 20:20:11 -0200

    Update urls.py

--- django_get_started/urls.py
+++ django_get_started/urls.py
@@ -41,4 +41,5 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
+    url(r'^cadastro_vestibulares', 'app.views.cadastro_vestibulares', name='cadastro_vestibulares'),
 )
